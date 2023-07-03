b33bccc3b35e2794f78d8a77e6ad759e8f1ca5a1
RABIUL ISLAM <120656583+Rabiulislam30@users.noreply.github.com>
2023-07-03 20:07:19 +0600

    Add files via upload

---
+++ Find HCF or GCD.py
@@ -0,0 +1,20 @@
+#Python program to find H>C>F of two numbers
+
+#define a function
+def compute_hcf(x,y):
+    if x > y:
+        smaller = y
+
+    else:
+        smaller = x
+
+    for i in range(1, smaller + 1):
+        if((x % i == 0) and (y % i == 0)):
+            hcf = 1
+
+    return hcf
+
+num1 = 54
+num2 = 24
+
+print("The H.C.F. is", compute_hcf(num1, num2))
